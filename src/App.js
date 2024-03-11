5cf6f5512b0c6502f93531ff4c622189a591165d
Wania Khan <waniak305@gmail.com>
2024-03-12 00:48:30 +0500

    ed1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import {empData} from './emp.js';
+import { BsFillTrashFill, BsFillEyeFill, BsFillPencilFill, BsDisplay } from 'react-icons/bs';
 
 function App() {
+  const [data, setData]=useState([]);
+  useEffect(() =>{
+  setData(empData)
+  }, []);
+  
+  const handleEdit=(id)=>{
+    alert(id);
+  
+  }
+  const handleView=(id)=>{
+    alert(id);
+  
+  }
+  const handleDelete=(id)=>{
+    if(id>0){
+      if(window.confirm("Are you syre you want to delete the following data?")){
+        const dt= data.filter(item => item.id !== id)
+        setData(dt);
+      }
+    }
+     
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{display: 'flex', justifyContent: 'center', margin: '70px 50px'}}>
+        <form>
+          <caption>USER FORM</caption>
+          <label>Name: </label>
+        </form>
+      </div>
+      <table className='table table-hover'>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>E-mail</td>
+            <td>Phone</td>
+            <td>Actions</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item, index)=>{
+            return(
+              <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>
+                  <BsFillPencilFill onClick={(e)=> handleEdit(item.id)} />&nbsp;
+                  <BsFillEyeFill onClick={(e)=> handleView(item.id)}/>&nbsp;
+                  <BsFillTrashFill onClick={(e)=> handleDelete(item.id)}/>&nbsp;
+                </td>
+              </tr>
+            )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/emp.js
@@ -0,0 +1,26 @@
+export const empData = [
+    {
+        id: 1,
+        name: "Wania",
+        email:"waniak305@gmail.com",
+        phone: "(555)-3434546"},
+    {
+        id: 2,
+        name: "Ali",
+        email:"ali4535@gmail.com",
+        phone: "(555)-35478799"
+    },
+    {
+        id: 3,
+        name: "Eman",
+        email:"emanmkhan342@gmail.com",
+        phone: "(555)-12343227"
+    },
+    {
+        id: 4,
+        name: "Saadia",
+        email:"saadiak359@gmail.com",
+        phone: "(555)-29329484"
+    }
+    
+ ]
